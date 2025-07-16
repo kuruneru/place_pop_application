e29958097b239d4e8e6e935c3315678149bd3bec
kuruneru <kuruneru@yamadatakayuki.localdomain>
2025-07-17 03:18:06 +0900

    修正

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from starlette.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from dotenv import load_dotenv
 import os
+
+
+app = FastAPI()
+db_url = os.getenv("DATABASE_URL")
