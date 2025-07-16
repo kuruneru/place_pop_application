13f9fba24ed0ce00a1174c315925d82ba8f0637c
kuruneru <kuruneru@yamadatakayuki.localdomain>
2025-07-17 03:51:06 +0900

    仮ページ

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def first(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "messages": messages})
+    return templates.TemplateResponse("index.html", {"request": request, "messages": "messages"})
