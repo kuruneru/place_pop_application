c1ffc6289d3a1b43142466ce7b851a1e8bdcf565
kuruneru <kuruneru@yamadatakayuki.localdomain>
2025-08-07 07:07:01 +0900

    cloudinary

--- app.py
+++ app.py
@@ -9,6 +9,29 @@ import os
 import uuid
 import shutil
 import bcrypt
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = "djlgesfne", 
+    api_key = "121659728684422", 
+    api_secret = "<your_api_secret>", # Click 'View API Keys' above to copy your API secret
+    secure=True
+)
+
+# Upload an image
+upload_result = cloudinary.uploader.upload("pop_application")
+print(upload_result["secure_url"])
+
+# Optimize delivery by resizing and applying auto-format and auto-quality
+optimize_url, _ = cloudinary_url("shoes", fetch_format="auto", quality="auto")
+print(optimize_url)
+
+# Transform the image: auto-crop to square aspect_ratio
+auto_crop_url, _ = cloudinary_url("shoes", width=500, height=500, crop="auto", gravity="auto")
+print(auto_crop_url)
 
 #定義やインスタンス化
 app = FastAPI()
