c530f25fd3e093a2fc5923c7ed5e1548d55cc119
kuruneru <kuruneru.ikkou@gmail.com>
2025-08-22 00:14:51 +0900

    post_page_comment

--- app.py
+++ app.py
@@ -215,4 +215,4 @@ async def post_detail(request: Request, post_id: str):
     if not post:
         raise HTTPException(status_code=404, detail="投稿が見つかりません")
 
-    return templates.TemplateResponse("/post_detail.html", {"request": request, "post": dict(post._mapping)})
+    return templates.TemplateResponse("post_detail.html", {"request": request, "post": dict(post._mapping)})
