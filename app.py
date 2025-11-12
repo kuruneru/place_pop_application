f21038089e2851bf4aa00eea2fc6ee239fbc8999
kuruneru <kuruneru.ikkou@gmail.com>
2025-11-12 21:35:09 +0900

    post_eva

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ async def first(request: Request):
         result = conn.execute(text("SELECT posts.id, posts.title, posts.place_name, posts.address, posts.image_filename, posts.poster_type, posts.location_type, posts.created_at, posts.updated_at, posts.annotation, users.username AS user_name FROM posts JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC"))
         rows = result.fetchall()
         for row in rows:
+            print(f">> {row[i](0)}")
             i = 0
             user_id = row[i](0)
             local_result = conn.execute(text("SEELCT COUNT(*) FROM evaluations WHERE evaluation_type = 'local'"))
